3cbb638a8f981c2917ebc3bec1d75233e116bf2d
Vasudhevb <168171622+Vasudhevb@users.noreply.github.com>
2025-05-13 16:57:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "vasu" {
+resource "aws_instance" "dev" {
     ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.nano"
     tags = {
